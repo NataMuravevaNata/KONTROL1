5ead94266af75e24a4cd198abe11aaf67f558f7d
Nata <muravevantsh@mail.ru>
2022-09-20 00:48:01 +0300

    Example

---
+++ Example/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив 
+//из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+//выполнения алгоритма. При решении не рекомендуется 
+//пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void FillArray(string[] array)
+{
+    Console.WriteLine("Введите массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        string text = Console.ReadLine() ?? "0";
+        array[i] = text;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write($"{array[i]} ");
+}
+
+void ChangeArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+     }
+    
+}
+
